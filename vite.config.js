43c7a541bae6efe50529e33f74d146565d1cd4e2
rahman <rahmannurhayadi@gmail.com>
2024-08-01 11:09:47 +0700

    fix:add base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/UMKMCONNECT_FE/', // Pastikan base berada di sini
+  base: '/umkmconnect_fe/', // Pastikan base berada di sini
   plugins: [
     vue(),
   ],
