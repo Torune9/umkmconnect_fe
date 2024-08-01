38cd26f91d3880e0b79642eb5fe964f1c653de6e
rahman <rahmannurhayadi@gmail.com>
2024-08-01 10:33:57 +0700

    fix:add base url

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/UMKMCONNECT_FE/', // Pastikan base berada di sini
   plugins: [
     vue(),
   ],
   resolve: {
-    base: '/UMKMCONNECT_FE/',
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
