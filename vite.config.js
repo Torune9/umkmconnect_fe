a1d0472b0796d8e739b27f70f8ae3721a563f957
rahman <rahmannurhayadi@gmail.com>
2024-08-01 18:44:08 +0700

    fix : add dist dir

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/umkmconnect_fe/', // Pastikan base berada di sini
   plugins: [
     vue(),
   ],
