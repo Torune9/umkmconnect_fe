13d67211b7ecc5d8d3695d670b8da45e21dd09d6
rahman <rahmannurhayadi@gmail.com>
2024-08-06 12:11:29 +0700

    fix : fix image user

--- src/views/layouts/ProfileUserView.vue
+++ src/views/layouts/ProfileUserView.vue
@@ -12,7 +12,7 @@
                     <div class="relative flex flex-col items-center justify-center">
                         <div class="w-32 h-32 max-sm:h-24 max-sm:w-24  rounded-full overflow-hidden flex justify-center items-center ">
                            <div class="h-full w-full flex justify-center items-center" v-if="src || userData.image">
-                            <img  :src="src ? src : userData.image" alt="" class="w-full h-full">
+                            <img  :src="src ? src : userData.image" alt="" class="w-90 h-90">
                            </div>
                             <div v-else class="bg-yellow-400 w-full h-full text-white flex justify-center items-center text-7xl max-sm:text-4xl " >
                                 {{ userData.username.charAt(0).toUpperCase() }}
