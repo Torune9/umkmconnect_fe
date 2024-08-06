20396d821eabe28ffdca07b59585b3871f71cf7e
rahman <rahmannurhayadi@gmail.com>
2024-08-06 18:24:59 +0700

    fix:fix modalTask

--- src/components/modal/taskModal.vue
+++ src/components/modal/taskModal.vue
@@ -1,6 +1,6 @@
 <template>
     <Transition name="modals">
-        <div v-if="isShow" class="fixed flex h-screen w-screen justify-center items-center z-50">
+        <div v-if="isShow" class="fixed flex h-full w-full justify-center items-center z-50 bg-black/40  ">
             <form @submit.prevent="createWorkOrTask" class="flex flex-col justify-center border gap-2 p-4 relative bg-white shadow-md">
                 <h1 class="text-2xl font-semibold">
                     {{ isWorkspace ? 'Create  Workspace' : 'Task Create' }}
